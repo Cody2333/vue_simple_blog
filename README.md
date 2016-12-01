1fbc1446cc616fa8e14c64f4e86e1f093ac6a68c
cody <wangkedi.wang@gmail.com>
2016-12-01 22:46:04 +0800

    change path;update readme

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # vue-static-blog
 
-> cody's blog
+> simple blog built with vue2.0
+
+## Introduction
+
+## Features
+- 用leancloud存文件，并且使用leancloud的rest api 做文件的读写
+- 用github 提供的解析markdown to html 的api 解析
+- .md文件额外头部：
+
+```
+// xx.md
+//头部用yaml语法解析一些信息
+---
+title: my first post
+date: 2016-07-02 22:08:32
+tags:
+---
+...
+```
 
 ## Build Setup
 
@@ -14,5 +32,3 @@ npm run dev
 # build for production with minification
 npm run build
 ```
-
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
-import About from './About.vue';
-import PostDetail from './PostDetail.vue';
-import Posts from './Posts.vue';
-import Archives from './Archives.vue';
+import About from './components/About.vue';
+import PostDetail from './components/PostDetail.vue';
+import Posts from './components/Posts.vue';
+import Archives from './components/Archives.vue';
 import Admin from './Admin.vue';
 import Login from './components/admin/Login.vue';
 import List from './components/admin/List.vue';
-import {
-  getPosts,
-} from './utils/utils';
 
 Vue.use(VueRouter);
-getPosts();
 const routes = [{
   path: '/',
   component: App,
